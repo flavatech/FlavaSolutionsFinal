ad3159b14350bf4890c2b18df5c348f7091b1990
Okunsanya Smith <administrator@gcfc.edu.jm>
2019-08-08 17:14:40 -0500

    Email Confirmation, Reset Password, Forgot Password, users and Roles added

---
+++ FlavaSolutionsFinal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FlavaSolutionsFinal.Startup))]
+namespace FlavaSolutionsFinal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
